b9dc8a7e93a8318e58459a4ed693bef245a3e9fb
kazi.hassan <kazi.hassan@CRT-13PY903.alexgov.net>
2022-04-11 15:26:22 -0400

    changed

---
+++ src/application/App.java
@@ -0,0 +1,21 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		int counter=1;
+		
+		//while (true) {
+			
+		while (counter<6) {
+			System.out.printf("counter: %d\n", counter);
+			
+		counter=counter+1;
+		
+			
+		}
+
+	}
+
+}
